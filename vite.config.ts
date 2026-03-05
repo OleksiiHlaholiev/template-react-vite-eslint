b4daeef90689861fc1a8c48662518d31ab2abbf6
glalex <oleksii.hlaholiev@gmail.com>
2026-03-05 18:49:23 +0200

    vite.config.ts:: added checker-overlay config-props-styles

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,12 @@ export default defineConfig({
 			},
 			typescript: {
 				tsconfigPath: 'tsconfig.app.json'
+			},
+			overlay: {
+				initialIsOpen: true,
+				position: 'tr',
+				panelStyle: 'height: 100%',
+				badgeStyle: 'min-width: 150px'
 			}
 		})
 	]
