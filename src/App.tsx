1400dadb608ebf3c1c8069b5e4cb1c475c025010
glalex <oleksii.hlaholiev@gmail.com>
2026-03-06 11:52:44 +0200

    App.tsx:: added test type/func/code (newJsFeaturesFunc, like Object.groupBy)

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,28 @@ const myFunc = (
 	return input
 }
 
+const newJsFeaturesFunc = () => {
+	const inventory = [
+		{ name: 'asparagus', type: 'vegetables', quantity: 5 },
+		{ name: 'bananas', type: 'fruit', quantity: 0 },
+		{ name: 'goat', type: 'meat', quantity: 23 },
+		{ name: 'cherries', type: 'fruit', quantity: 5 },
+		{ name: 'fish', type: 'meat', quantity: 22 }
+	]
+
+	const result = Object.groupBy(inventory, ({ type }) => type)
+
+	console.log('newJsFeaturesFunc', { inventory, result })
+}
+
 function App() {
 	const [count, setCount] = useState<number>(0)
 
 	const test = { x: 50, y: 'test', z: true }
 
 	myFunc(test)
+
+	newJsFeaturesFunc()
 
 	return (
 		<>
